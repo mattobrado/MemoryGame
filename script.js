b4a33c3755b5e6ea89d8da6a9aeb18563c6e966d
mattobrado <mcobradovic@gmail.com>
2022-05-15 10:15:25 -0500

    feat: completed game

--- script.js
+++ script.js
@@ -13,6 +13,17 @@ const COLORS = [
   "purple"
 ];
 
+// firstCard holds the first card we are finding a match for
+// secondCard holds our guess of which card matches first card
+// initilaized to null because we haven't clicked a card yet
+let firstCard = null;
+let secondCard = null;
+// cardsToHide holds cards we want to hide later
+// so that we can re-assign firstCard and secondCard
+let cardsToHide = [];
+// we need a global timout variable to be able to clear the timer at any time
+let timeout;
+
 // here is a helper function to shuffle an array
 // it returns the same array with values shuffled
 // it is based on an algorithm called Fisher Yates if you want ot research more
@@ -57,10 +68,93 @@ function createDivsForColors(colorArray) {
   }
 }
 
+// function to hide card
+function hideCard(card){
+  // if we got passed null of undefined do nothing
+  if (card == null){
+    return;
+  }
+  // if we got passed a matched card do nothing
+  if (card.getAttribute('data-matched')){
+    return;
+  }
+  // set background color of card to null
+  card.style.backgroundColor = null;
+}
+
+// function to revael card
+function revealCard(card){
+  // set backgroundcolor to className
+  card.style.backgroundColor = card.className;
+}
+
 // TODO: Implement this function!
 function handleCardClick(event) {
+
   // you can use event.target to see which element was clicked
-  console.log("you just clicked", event.target);
+  const clickedCard = event.target;
+  console.log("you just clicked", clickedCard);
+
+  // if the clicked card is already matched, do not coninue (return)
+  if (clickedCard.getAttribute('data-matched')){
+    console.log("You already matched that card");
+    return;
+  }
+
+  // if we have a value in firstCard
+  if (firstCard !== null) {
+
+    // if second card is not the exact same card
+    if (clickedCard !== firstCard) {
+
+      // store clickedCard in secondCard
+      secondCard = clickedCard;
+      // reveal card
+      revealCard(secondCard);
+
+      // check for match by camparing classNames
+      if (firstCard.className === secondCard.className){
+
+        // match found
+        console.log("It's a Match!")
+
+        // set matched data attribute so we can't choose these cards again
+        firstCard.setAttribute('data-matched',true);
+        secondCard.setAttribute('data-matched',true);
+      }
+      else {
+        
+        // hide cards
+        // we need to store the cards we want to hide in an array 
+        // because we will be setting the references 
+        // firstCard, secondCard = null befrore the timout function
+        // is called
+        cardsToHide.push(firstCard);
+        cardsToHide.push(secondCard);
+        // shift out  2 elements from front, cardsToHide acts as a queue
+        timeout = setTimeout(function() {
+          hideCard(cardsToHide.shift());
+          hideCard(cardsToHide.shift());
+        }, 1000);
+      }
+
+      // reset first and second card to null, so we can start over
+      firstCard = null;
+      secondCard = null;
+    }
+  }
+
+  // there is no card stored in firstCard
+  else {
+    // hide cards imediately
+    clearTimeout(timeout);
+    hideCard(cardsToHide.shift());
+    hideCard(cardsToHide.shift());
+    // store clickedCard
+    firstCard = clickedCard;
+    // reveal card
+    revealCard(firstCard);
+  }
 }
 
 // when the DOM loads
